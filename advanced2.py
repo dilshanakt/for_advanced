44e39a849630b87c8221611be08576793cb63ac4
dilshanakt <110896961+dilshanakt@users.noreply.github.com>
2022-08-10 14:15:16 +0530

    first commit

---
+++ advanced2.py
@@ -0,0 +1,18 @@
+for_list=[1,2,3,4,5,6,7,8,9,10]
+for x in for_list:
+    print(x)
+    print(x*x)
+    print(x*x*x)
+    print(x*x*x*x)
+
+
+print("Outside the loop")
+
+list=[["thanha",23,"place","Luminar"],["thanha",23,"place","luminar"]]
+for i in list:
+    print(i[0],"is a good student")
+    print(i[1],"is the age of student")
+    print(i[2],"is the place of student")
+    print(i[3],"is the institute of student")
+    print("\n")
+    print("Next student")
