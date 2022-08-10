c783de0fe7efe0c7f76f38eb48967b13d808af34
dilshanakt <110896961+dilshanakt@users.noreply.github.com>
2022-08-10 14:14:54 +0530

    first commit

---
+++ advanced1.py
@@ -0,0 +1,12 @@
+remove_list=[1,2,3,4,5,"Hello"]
+print(remove_list)
+remove_list.remove(1)#remove value
+print(remove_list)
+remove_list.remove(2)#remove value
+print(remove_list)
+remove_list.pop(0)#remove index
+print(remove_list)
+remove_list.pop()#remove last value
+print(remove_list)
+del remove_list[0]#index dlt
+print(remove_list)
